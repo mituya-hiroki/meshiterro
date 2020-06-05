8159ecbf7de6b6818996407057f9ac070ed6cb07
mituya-hiroki <soccer.g.osaka.11@gmail.com>
2020-06-05 17:57:55 +0900

    finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'd7093bd5772ca18b0c1e801ac34bbf617de6a23ca2da162fa25bef38ba13a0ec63672bfabfc0df267fbf4784aed14ecad0aa01747631b84b2305870605158ee2'
